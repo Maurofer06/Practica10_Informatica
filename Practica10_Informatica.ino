bd5a3fd2d123b2e733b3d80ee3ca85025245cb2c
Maurofer06 <maurolustres14@gmail.com>
2024-12-12 10:12:50 +0100

    Create Practica10_Informatica.ino

---
+++ Practica10_Informatica.ino
@@ -0,0 +1,25 @@
+/*
+ * Esta es una practica de encender alternativamente dos LEDS 
+ * Autor: Mauro Fernandez 
+ * Data: 12/12/2024
+*/
+
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(4000); // Wait for 4000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(2000); // Wait for 2000 millisecond(s)
+  digitalWrite(12, HIGH);
+  delay(4000); // Wait for 4000 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(2000); // Wait for 2000 millisecond(s)
+}
